8f68a18fb654805cddcbe5e796bbfa23bcd01c91
Iksan Arisandi <iksanarisandi@gmail.com>
2025-12-14 17:40:24 +0800

    chore: add netlify adapter for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import { netlify } from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   server: {
@@ -15,5 +16,6 @@ export default defineConfig({
       srcDirectory: 'src',
     }),
     viteReact(),
+    netlify(),
   ],
 })
